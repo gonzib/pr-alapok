d21d284751638ad207e68ac5f5f46e1faec166c2
gonczib <113088552+gonzib@users.noreply.github.com>
2023-10-05 22:32:34 +0200

    Add files via upload

---
+++ hazi_feladatok/hazi_feladat_001.py
@@ -0,0 +1,10 @@
+# hazi_feladat_001
+"""
+Készíts egy programot, amely megkérdezi a felhasználótól, hogy jó napja van-e! A válasz kétféle lehet: igen vagy nem. 
+A választól függően írjon ki üzenetet a gép!
+"""
+nap = input('Jó napod volt? ')
+if nap == "igen":
+    print(f"azjo")
+elif nap == "nem":
+    print(f"aznemjo")
