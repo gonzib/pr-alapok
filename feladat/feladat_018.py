980baded452e215c4a567ec0da9339fdce0b95cb
gonczib <113088552+gonzib@users.noreply.github.com>
2023-11-10 13:24:40 +0100

    Update feladat_018.py

--- feladat/feladat_018.py
+++ feladat/feladat_018.py
@@ -3,6 +3,6 @@
 
 szo = None
 while szo != '':
-    szo = input('Adj meg szavakat! Ha kilépnél, aszó helyett csak egy ENTER-t üss! ')
+    szo = input('Adj meg szavakat! Ha kilépnél, a szó helyett csak egy ENTER-t üss! ')
 
 print('Program vége!')
