5920150e1acaf2fcf5e77cb3a8d8899fd75dfdac
gonczib <113088552+gonzib@users.noreply.github.com>
2023-11-17 13:08:57 +0100

    Update feladat_021.py

--- feladat/feladat_021.py
+++ feladat/feladat_021.py
@@ -7,7 +7,7 @@ while sor <= 7:
     oszlop = 1
     while oszlop <= darab_karakter:
         print('O ', end='')
-        oszlop = oszlop+1
+        oszlop = oszlop + 1
     print('')
     darab_karakter =darab_karakter + 1
     sor = sor + 1
